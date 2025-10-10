e0b6250dcf71e976563b9aaf0d388f307076a122
shivamdotdev <shivamdotdev01@gmail.com>
2025-10-10 19:43:37 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeatureSection from '../components/FeatureSection'
+import TrailerSections from '../components/TrailerSections'
+
+const Home = () => {
+  return (
+    <>
+    <HeroSection />
+    <FeatureSection />
+    <TrailerSections />
+    
+      
+    </>
+  )
+}
+
+export default Home
